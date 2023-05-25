a01bcfd0263fd5d7d26f3843ab33fe4720489128
chojangg <s2115@e-mirim.hs.kr>
2023-05-25 14:55:08 +0900

    재귀 방식으로 구현

---
+++ RecursionPrac/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPrac
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(30));
+        }
+    }
+}
--- RecursionPrac/Program.cs
+++ RecursionPrac/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Runtime.CompilerServices;
 
 namespace RecursionPrac
 {
-    internal static class Program
+    internal class Fibonacci
     {
-        
+        public long Get(int n)
+        {
+            Console.Write(n + " ");
+            if (n < 0) { return 0; }
+            if (n == 1) { return 1; }
+            return Get(n - 2) + Get(n - 1);
+        }
     }
 }
